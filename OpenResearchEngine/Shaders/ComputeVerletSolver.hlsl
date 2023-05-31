c25327ec6e284ce198ed389a3d49b346cc6a916e
JosephChittock <josephchittock@gmail.com>
2023-05-31 22:44:48 +1200

    add in comments

--- OpenResearchEngine/Shaders/ComputeVerletSolver.hlsl
+++ OpenResearchEngine/Shaders/ComputeVerletSolver.hlsl
@@ -31,22 +31,26 @@ void CS(uint3 dispatchThreadID : SV_DispatchThreadID)
     
     uint vertexID = dispatchThreadID.x;
    
+    // Retrieve the input and skinned vertex positions
     float3 inputVertexPosition = inputVertexBuffer[vertexID].position;
     float3 skinnedVertexPosition = skinnedVertexBuffer[vertexID].position;
+    
+    // add the previous frames solve to the new skinned position to initialize the current frames solve
     outputVertexBuffer[vertexID].position = skinnedVertexPosition + springTransformBuffer[vertexID].transform;
     
     // Set the Pre Solve init positions for the outputBuffer
-    for (int i = 0; i < 8; ++i)
+    for (int i = 0; i < neighbourCount; ++i)
     {
         uint neighbour = vertexAdjacencyBuffer[vertexID].index[i];
         
         if (neighbour != vertexID)
         {
+            // Set the initial positions for neighboring vertices to prepare for the current frames solve
             outputVertexBuffer[neighbour].position = skinnedVertexBuffer[neighbour].position + springTransformBuffer[neighbour].transform;
         }
     }
     
-    // Apply the constraints
+    // Solve for the current frame
     for (int i = 0; i < springIterations; ++i)
     {
         for (int i = 0; i < neighbourCount; ++i)
@@ -55,19 +59,21 @@ void CS(uint3 dispatchThreadID : SV_DispatchThreadID)
         
             if (neighbour != vertexID)
             {
-            
+                // Calculate displacement and lengths for constraints
                 float3 constraintDisplacement = inputVertexBuffer[vertexID].position - inputVertexBuffer[neighbour].position;
                 float3 constraintSkinnedDisplacement = outputVertexBuffer[vertexID].position - outputVertexBuffer[neighbour].position;
             
                 float constraintLength = length(constraintDisplacement);
                 float constraintSkinnedLength = length(constraintSkinnedDisplacement);
                 
+                // Calculate scale and correction vector
                 float scale = constraintLength / constraintSkinnedLength;
-            
                 float3 correctionVector = (constraintSkinnedDisplacement * (1.0 - scale)) * 0.5;
             
+                // Apply the correction vectors if scale is greater than epsilon
                 if (scale > 1e-6)
                 {
+                    // Apply the correction vectors to the positions of the vertices
                     outputVertexBuffer[vertexID].position += -correctionVector;
                     outputVertexBuffer[neighbour].position += correctionVector;
                 }
@@ -75,18 +81,19 @@ void CS(uint3 dispatchThreadID : SV_DispatchThreadID)
         }
     }
     
-    // Pass the relative solver data to the spring transform buffer so it can initialize the next frame
-    for (int i = 0; i < 8; ++i)
+    // Pass the relative solver data to the spring transform buffer so it can initialize the solve during the next frame
+    for (int i = 0; i < neighbourCount; ++i)
     {
         uint neighbour = vertexAdjacencyBuffer[vertexID].index[i];
         
         if (neighbour != vertexID)
         {
+            // Calculate the constraint skinned vertex position
             float3 constraintSkinnedVertexPosition = skinnedVertexBuffer[neighbour].position;
             
+            // Update the spring transform buffer with the relative transformations
             springTransformBuffer[vertexID].transform = outputVertexBuffer[vertexID].position - skinnedVertexPosition;
             springTransformBuffer[neighbour].transform = outputVertexBuffer[neighbour].position - constraintSkinnedVertexPosition;
-
         }
     }
 }
