3c14ce46c85c4ec3d492c287852166014648347b
JChittockDev <josephchittock@gmail.com>
2023-07-30 21:37:01 +1200

    quantize functions

--- OpenResearchEngine/Shaders/ComputePostSolve.hlsl
+++ OpenResearchEngine/Shaders/ComputePostSolve.hlsl
@@ -6,16 +6,21 @@ struct Vertex
     float4 tangent;
 };
 
-
-StructuredBuffer<Vertex> simMeshSkinnedVertexBuffer : register(t0);
-StructuredBuffer<uint3> simMeshSpringTransformBuffer : register(t1);
+StructuredBuffer<uint3> simMeshSpringTransformBuffer : register(t0);
 RWStructuredBuffer<Vertex> simMeshTransformedVertexBuffer : register(u0);
+
+float3 UnQuantize(uint3 input, float factor)
+{
+    float vertexPositionX = ((float) input.x) / factor;
+    float vertexPositionY = ((float) input.y) / factor;
+    float vertexPositionZ = ((float) input.z) / factor;
+    return float3(vertexPositionX, vertexPositionY, vertexPositionZ);
+}
 
 // Define the compute shader entry point
 [numthreads(64, 1, 1)]
 void CS(uint3 dispatchThreadID : SV_DispatchThreadID)
 {
     uint simMeshVertexID = dispatchThreadID.x;
-    float3 simMeshSkinnedVertexPosition = simMeshSkinnedVertexBuffer[simMeshVertexID].position;
-    //simMeshSpringTransformBuffer[simMeshVertexID].transform = simMeshTransformedVertexBuffer[simMeshVertexID].position - simMeshSkinnedVertexPosition;
+    simMeshTransformedVertexBuffer[simMeshVertexID] += UnQuantize(simMeshSpringTransformBuffer[simMeshVertexID], QUANTIZE);
 }
--- OpenResearchEngine/Shaders/ComputePreSolve.hlsl
+++ OpenResearchEngine/Shaders/ComputePreSolve.hlsl
@@ -10,12 +10,20 @@ StructuredBuffer<Vertex> simMeshSkinnedVertexBuffer : register(t0);
 StructuredBuffer<uint3> simMeshSpringTransformBuffer : register(t1);
 RWStructuredBuffer<Vertex> simMeshTransformedVertexBuffer : register(u0);
 
+float3 UnQuantize(uint3 input, float factor)
+{
+    float vertexPositionX = ((float) input.x) / factor;
+    float vertexPositionY = ((float) input.y) / factor;
+    float vertexPositionZ = ((float) input.z) / factor;
+    return float3(vertexPositionX, vertexPositionY, vertexPositionZ);
+}
+
 // Define the compute shader entry point
 [numthreads(64, 1, 1)]
 void CS(uint3 dispatchThreadID : SV_DispatchThreadID)
 {
     uint simMeshVertexID = dispatchThreadID.x;
     float3 simMeshSkinnedVertexPosition = simMeshSkinnedVertexBuffer[simMeshVertexID].position;
-    
-    simMeshTransformedVertexBuffer[simMeshVertexID].position = simMeshSkinnedVertexPosition + simMeshSpringTransformBuffer[simMeshVertexID].transform;
+    float3 simMeshSpringTransform = UnQuantize(simMeshSpringTransformBuffer[simMeshVertexID], QUANTIZE);
+    simMeshTransformedVertexBuffer[simMeshVertexID].position = simMeshSkinnedVertexPosition + simMeshSpringTransform;
 }
