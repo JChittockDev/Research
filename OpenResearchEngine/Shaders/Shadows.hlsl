9689fd8bb65ae4a9f41d98aa5910b18cf72801c5
JChittockDev <josephchittock@gmail.com>
2025-04-14 19:34:29 -0700

    removing extra hlsl crap

--- OpenResearchEngine/Shaders/Shadows.hlsl
+++ OpenResearchEngine/Shaders/Shadows.hlsl
@@ -1,9 +1,97 @@
-//***************************************************************************************
-// Shadows.hlsl by Frank Luna (C) 2015 All Rights Reserved.
-//***************************************************************************************
+#ifndef NUM_DIR_LIGHTS
+    #define NUM_DIR_LIGHTS 0
+#endif
 
-// Include common HLSL code.
-#include "Common.hlsl"
+#ifndef NUM_POINT_LIGHTS
+    #define NUM_POINT_LIGHTS 0
+#endif
+
+#ifndef NUM_SPOT_LIGHTS
+    #define NUM_SPOT_LIGHTS 3
+#endif
+
+#define MaxLights 16
+
+struct Light
+{
+    float3 Strength;
+    float FalloffStart; // point/spot light only
+    float3 Direction; // directional/spot light only
+    float FalloffEnd; // point/spot light only
+    float3 Position; // point light only
+    float InnerConeAngle; // spot light only
+    float OuterConeAngle; // spot light only
+    float Pad1;
+    float Pad2;
+    float Pad3;
+};
+
+struct MaterialData
+{
+    float4 DiffuseAlbedo;
+    float3 FresnelR0;
+    float Roughness;
+    float4x4 MatTransform;
+    uint DiffuseMapIndex;
+    uint NormalMapIndex;
+    uint RoughnessMapIndex;
+    uint MetalnessMapIndex;
+    uint SpecularMapIndex;
+    uint HeightMapIndex;
+    uint OpacityMapIndex;
+    uint OcclusionMapIndex;
+    uint RefractionMapIndex;
+    uint EmissiveMapIndex;
+    uint SubsurfaceMapIndex;
+    uint MatPad1;
+};
+
+TextureCube gCubeMap : register(t0);
+Texture2D gShadowMap[16] : register(t1);
+Texture2D gSsaoMap : register(t17);
+Texture2D gTextureMaps[48] : register(t18);
+
+StructuredBuffer<MaterialData> gMaterialData : register(t0, space1);
+
+SamplerState gsamPointWrap : register(s0);
+SamplerState gsamPointClamp : register(s1);
+SamplerState gsamLinearWrap : register(s2);
+SamplerState gsamLinearClamp : register(s3);
+SamplerState gsamAnisotropicWrap : register(s4);
+SamplerState gsamAnisotropicClamp : register(s5);
+SamplerComparisonState gsamShadow : register(s6);
+
+cbuffer cbPerObject : register(b0)
+{
+    float4x4 gWorld;
+    float4x4 gTexTransform;
+    uint gMaterialIndex;
+    uint gObjPad0;
+    uint gObjPad1;
+    uint gObjPad2;
+};
+
+cbuffer cbPass : register(b1)
+{
+    float4x4 gView;
+    float4x4 gInvView;
+    float4x4 gProj;
+    float4x4 gInvProj;
+    float4x4 gViewProj;
+    float4x4 gInvViewProj;
+    float4x4 gViewProjTex;
+    float4x4 gShadowTransform[MaxLights];
+    float3 gEyePosW;
+    float cbPerObjectPad1;
+    float2 gRenderTargetSize;
+    float2 gInvRenderTargetSize;
+    float gNearZ;
+    float gFarZ;
+    float gTotalTime;
+    float gDeltaTime;
+    float4 gAmbientLight;
+    Light gLights[MaxLights];
+};
 
 struct VertexIn
 {
